4c3f0961aa92db501bb91ca99460a594f061c2d0
Rohit Bhosale <rohitbhosale409@GMAIL.COM>
2025-05-02 20:02:14 +0530

    some changes done

---
+++ script.js
@@ -0,0 +1,24 @@
+const imgs = document.querySelectorAll('.header-slider ul img');
+const prev_btn = document.querySelectorAll('.control_prev');
+const next_btn = document.querySelectorAll('.control_next');
+
+let n = 0;
+
+function changesSlide() {
+    for (let i = 0; i < imgs.length; i++) {
+        imgs[i].style.display = 'none';
+    }
+    imgs[n].style.display = 'block';
+}
+
+changesSlide();
+
+next_btn.forEach(btn => btn.addEventListener('click', () => {
+    n = (n + 1) % imgs.length;
+    changesSlide();
+}));
+
+prev_btn.forEach(btn => btn.addEventListener('click', () => {
+    n = (n - 1 + imgs.length) % imgs.length;
+    changesSlide();
+}));
